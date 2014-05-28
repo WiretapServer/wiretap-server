fd7df862cdcccf8abb57d380fae3f0b4c6df2c4a
Ali Hamidi <ahamidi@me.com>
2014-05-28 11:46:25 +0400

    added leaderboards list, create and read

--- routes/leaderboard.rb
+++ routes/leaderboard.rb
@@ -1,11 +1,35 @@
 class WiretapServer < Sinatra::Application
 
   get "/leaderboards.json" do
-    return jr(200, "Leaderboards list")
+    lb_list = Array.new
+    # Iterate across resulting data set and insert values
+    Leaderboard.dataset.each{ |row|
+      lb_list.insert(-1, row.values)
+    }
+    response = {:leaderboards => lb_list}
+    return jr(200, response)
+  end
+
+  post "/leaderboards.json" do
+    admin_protected!
+
+    request.body.rewind
+    data = JSON.parse request.body.read
+
+    lb = Leaderboard.create({
+        :name => data["name"],
+        :type => data["type"],
+        :reverse => data["reverse"]
+      })
+    return jr(201, "New Leaderboard Created: " << lb[:id].to_s)
   end
 
   get "/leaderboards/:id.json" do
     return jr(200, "Leaderboard " << params[:id])
   end
 
+  post "/leaderboards/:id/score.json" do
+    # Score submission
+    return jr(201, "Score Created!")
+  end
 end
